341ae59e3f233ac4db862bb16132e973ccdea435
Kim Juhyeong <nonconvexopt@gmail.com>
2021-12-26 23:37:27 +0900

    Make packages.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from glob import glob
+from os.path import basename, splitext
+from setuptools import find_packages, setup
+
+setup_requires = [
+    ]
+
+install_requires = [
+    'torch>=1.9.0',
+    'gpytorch>=1.6.0',
+    ]
+
+dependency_links = [
+    ]
+
+setup(
+    name='torch_ssge',
+    version='0.1',
+    author='Juhyeong Kim',
+    author_email='nonconvexopt@gmail.com',
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    dependency_links=dependency_links,
+    scripts=["ssge.py"],
+)
