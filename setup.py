ab81c4b4f54bc4a74cf844bdc00a995e43e30624
Kim Juhyeong <nonconvexopt@gmail.com>
2022-01-09 19:44:03 +0900

    Clean setup.py and apply new versioning.

--- setup.py
+++ setup.py
@@ -2,25 +2,19 @@ from glob import glob
 from os.path import basename, splitext
 from setuptools import find_packages, setup
 
-setup_requires = [
-    ]
 
 install_requires = [
     'torch>=1.9.0',
     'gpytorch>=1.6.0',
     ]
 
-dependency_links = [
-    ]
-
 setup(
     name='torch_ssge',
-    version='0.1',
+    description='PyTorch implementation of the Spectral Stein Gradient Estimator.',
+    version='0.1.0',
     author='Juhyeong Kim',
     author_email='nonconvexopt@gmail.com',
     python_requires='>=3.6',
     install_requires=install_requires,
-    setup_requires=setup_requires,
-    dependency_links=dependency_links,
     py_modules=['core'],
 )
