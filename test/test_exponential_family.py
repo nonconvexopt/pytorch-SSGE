a61ae7533bb4f9a50d50d8efeebcd1ee7a75fce4
Kim Juhyeong <nonconvexopt@gmail.com>
2022-01-12 16:08:18 +0900

    Automating the tests for analytical distribution

--- test/test_exponential_family.py
+++ test/test_exponential_family.py
@@ -1,3 +1,20 @@
 import pytest
 
-def test_
+
+# Example is consist of (sampler instance, <log density method>)
+EXAMPLES = [
+    torch.distributions.uniform.Uniform,
+    torch.distributions.laplace.Laplace,
+    torch.distributions.normal.Normal,
+    torch.distributions.poisson.Poisson,
+    torch.distributions.studentT.StudentT,
+    torch.distributions.gamma.Gamma,
+    torch.distributions.gumbel.Gumbel,
+    torch.distributions.dirichlet.Dirichlet,
+    torch.distributions.multivariate_normal.MultivariateNormal,
+    torch.distributions.multinomial.Multinomial,
+]
+
+
+def test_exponential_family():
+    assert 
